383ea184e4c1d482eb6f8216bc0c91db0dda5428
Your Name <bvbrunik@gmail.com>
2023-02-02 21:50:09 +0100

    App structure divided into basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 
-import './App.css';
+import {Map} from "./components/Map/Map"
+import {Header} from "./components/layout/Header";
 
 export const App = () =>  {
 
   return (
       <>
-      <header>
-          <h1>
-              <strong>Mega </strong>advertisement
-          </h1>
-
-<button>
-Add an advertisement
-</button>
-      </header>
-      <div className="map">
- <input type="text "/> <button>Search</button>
-      </div>
+            <Header/>
+             <Map/>
       </>
   );
 };
