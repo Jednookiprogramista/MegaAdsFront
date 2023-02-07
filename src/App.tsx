5aab6953b4c7260b4ac1086ac724fa78eabc1925
Your Name <bvbrunik@gmail.com>
2023-02-07 18:37:49 +0100

    Improve ad research in the header on the map.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {Map} from "./components/Map/Map"
 import {Header} from "./components/layout/Header";
+import {SearchContext} from "./contexts/search.contexts";
 
 export const App = () =>  {
 
+
+    const [search,setSearch] = useState('');
+
   return (
       <>
-            <Header/>
-             <Map/>
+            <SearchContext.Provider value={{search,setSearch}}>
+                <Header/>
+                <Map/>
+
+            </SearchContext.Provider>
       </>
   );
 };
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React, {useContext, useEffect} from "react";
 import './Map.css'
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
 import '../../utils/fix-map-icon';
+import {SearchContext} from "../../contexts/search.contexts";
 
 export const Map = () => {
+    const {search} = useContext(SearchContext)
+
+    useEffect(()=> {
+        console.log('Make request to search for,',search);
+    },[search]);
+
+
     return(
         <div className="map">
+            <h1>Search for:{search}</h1>
             <MapContainer center={[52.4026128,16.9073222]} zoom={20}>
                 <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
