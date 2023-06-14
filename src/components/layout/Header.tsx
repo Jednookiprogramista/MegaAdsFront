b728513ffe4dc6f82f34a826d5d90879fd5542b3
Bruno Braghieri <98560465+Jednookiprogramista@users.noreply.github.com>
2023-06-14 23:08:07 +0200

    added do github

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,6 +2,7 @@ import React, {SyntheticEvent, useContext, useState} from "react";
 import './Header.css'
 import {Btn} from "../common/Btn";
 import {SearchContext} from "../../contexts/search.contexts";
+import {Link} from "react-router-dom";
 
 export const Header = () => {
     const [inputVal, setInputVal] = useState('');
@@ -13,10 +14,14 @@ export const Header = () => {
     }
 
     return (
+
         <header>
-            <h1>
+            <Link to={'/'} style={{textDecoration: "none"}}>
+            <h1 >
                 <strong>Great</strong> advertisements
             </h1>
+            </Link>
+
             <Btn  to="/add" text="Add advertisement"/>
             <form className="search" onSubmit={setSearchFromLocalState}>
                 <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/><Btn text="Search"/>
